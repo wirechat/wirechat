9cc05e13562859df1e98534e7a7805fcf2e5f08f
Namu Makwembo <makwembonamu@gmail.com>
2024-11-12 18:02:10 +0300

    tested jobs data

---
+++ tests/Unit/Events/NotifyParticipantTest.php
@@ -0,0 +1,94 @@
+<?php
+
+use Illuminate\Support\Facades\Event;
+use Namu\WireChat\Events\MessageCreated;
+use Namu\WireChat\Events\MessageDeleted;
+use Namu\WireChat\Events\NotifyParticipant;
+use Namu\WireChat\Models\Conversation;
+use Namu\WireChat\Models\Message;
+use Workbench\App\Models\User;
+
+describe(" Data verifiction ", function () {
+
+    test('message id  is present', function () {
+
+        Event::fake();
+        $auth = User::factory()->create();
+        $receiver = User::factory()->create(['name' => 'John']);
+
+        $message = $auth->sendMessageTo($receiver,'hello');
+
+        $participant= $message->conversation->participant($receiver);
+
+        NotifyParticipant::dispatch($participant,$message);
+        Event::assertDispatched(NotifyParticipant::class, function ($event) use ($message) {
+
+            $broadcastMessage = (array) $event->broadcastWith();
+            expect($broadcastMessage['message']['id'])->toBe($message->id);
+
+            return $this;
+        });
+    });
+
+    test('conversation id is present', function () {
+
+        Event::fake();
+        $auth = User::factory()->create();
+        $receiver = User::factory()->create(['name' => 'John']);
+
+        $message = $auth->sendMessageTo($receiver,'hello');
+
+        $participant= $message->conversation->participant($receiver);
+
+        NotifyParticipant::dispatch($participant,$message);
+        Event::assertDispatched(NotifyParticipant::class, function ($event) use ($message) {
+
+            $broadcastMessage = (array) $event->broadcastWith();
+
+            expect($broadcastMessage['message']['conversation_id'])->toBe($message->conversation_id);
+            return $this;
+        });
+    });
+
+
+
+
+    it(' broadcasts on correct  private channnel', function () {
+        Event::fake();
+        $auth = User::factory()->create();
+        $receiver = User::factory()->create(['name' => 'John']);
+
+        $message = $auth->sendMessageTo($receiver,'hello');
+
+        $participant= $message->conversation->participant($receiver);
+
+
+        NotifyParticipant::dispatch($participant,$message);
+        Event::assertDispatched(NotifyParticipant::class, function ($event) use ($message,$participant) {
+
+            $broadcastOn =  $event->broadcastOn();
+            expect($broadcastOn[0]->name)->toBe("private-participant.".$participant->participantable_id);
+            return $this;
+        });
+    });
+
+
+    it(' broadcasts only on correct 1  private channnel', function () {
+        Event::fake();
+        $auth = User::factory()->create();
+        $receiver = User::factory()->create(['name' => 'John']);
+
+        $message = $auth->sendMessageTo($receiver,'hello');
+
+        $participant= $message->conversation->participant($receiver);
+
+        NotifyParticipant::dispatch($participant,$message);
+        Event::assertDispatched(NotifyParticipant::class, function ($event) use ($message) {
+
+            $broadcastOn =  $event->broadcastOn();
+            expect(count($broadcastOn))->toBe(1);
+            return $this;
+        });
+    });
+
+});
