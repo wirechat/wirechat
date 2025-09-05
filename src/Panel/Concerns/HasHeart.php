19f50734c7c7bfbaf4adb105c5fe1c18e45182c6
Namu Makwembo <makwembonamu@gmail.com>
2025-09-05 16:20:45 +0300

    lint

--- src/Panel/Concerns/HasHeart.php
+++ src/Panel/Concerns/HasHeart.php
@@ -4,20 +4,16 @@ namespace Wirechat\Wirechat\Panel\Concerns;
 
 use Closure;
 
-
 trait HasHeart
 {
     /**
      * Enable or disable option
      */
-
     protected bool|Closure $hasHeart = false;
 
     public function heart(bool|Closure $condition = true): static
     {
         $this->hasHeart = $condition;
-
-
         return $this;
     }
 
