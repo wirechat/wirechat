e72cf47bcab77cd003982cb23834fa034cf0be90
Namu Makwembo <makwebonamu@gmail.com>
2026-01-30 17:55:36 +0300

    lint

--- src/Livewire/Chat/Drawer.php
+++ src/Livewire/Chat/Drawer.php
@@ -42,7 +42,7 @@ class Drawer extends Component
 
         $this->drawerComponents[$id] = [
             'name' => $this->getComponentName($componentClass),
-            'attributes' => $arguments, // Deprecated
+            'attributes' => $arguments, // Deprecated todo:must be removed
             'arguments' => $arguments,
             'modalAttributes' => array_merge(
                 $componentClass::modalAttributes(), // Fetch reusable modal attributes
