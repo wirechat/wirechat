d66551fb4fc797fb6abd89c844607eb10a0a41a0
Namu Makwembo <makwembonamu@gmail.com>
2026-03-01 15:47:35 +0300

    ui:improved send message icon space

--- resources/views/livewire/chat/partials/footer.blade.php
+++ resources/views/livewire/chat/partials/footer.blade.php
@@ -504,20 +504,22 @@
                     <div x-cloak @class(['w-[5%] justify-end min-w-max  items-center gap-2 '])>
 
                         {{--  Submit button --}}
-                        <button
-                            x-show="((body?.trim()?.length>0) ||  $wire.media.length > 0 || $wire.files.length > 0 )"
-                            wire:loading.attr="disabled" wire:target="sendMessage" type="submit"
-                            id="sendMessageButton" class="cursor-pointer hover:text-[var(--wc-brand-primary)] transition-color ml-auto disabled:cursor-progress cursor-pointer font-bold">
+                        <div class="w-7 h-7">
+                            <button
+                                x-show="((body?.trim()?.length>0) ||  $wire.media.length > 0 || $wire.files.length > 0 )"
+                                wire:loading.attr="disabled" wire:target="sendMessage" type="submit"
+                                id="sendMessageButton" class="cursor-pointer hover:text-[var(--wc-brand-primary)] transition-color ml-auto disabled:cursor-progress cursor-pointer font-bold">
 
-                            <svg class="w-7 h-7   dark:text-gray-200" xmlns="http://www.w3.org/2000/svg"
-                                width="36" height="36" viewBox="0 0 24 24" fill="none"
-                                stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
-                                stroke-linejoin="round" class="ai ai-Send">
-                                <path
-                                    d="M9.912 12H4L2.023 4.135A.662.662 0 0 1 2 3.995c-.022-.721.772-1.221 1.46-.891L22 12 3.46 20.896c-.68.327-1.464-.159-1.46-.867a.66.66 0 0 1 .033-.186L3.5 15" />
-                            </svg>
+                                <svg class="w-7 h-7   dark:text-gray-200" xmlns="http://www.w3.org/2000/svg"
+                                    width="36" height="36" viewBox="0 0 24 24" fill="none"
+                                    stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                                    stroke-linejoin="round" class="ai ai-Send">
+                                    <path
+                                        d="M9.912 12H4L2.023 4.135A.662.662 0 0 1 2 3.995c-.022-.721.772-1.221 1.46-.891L22 12 3.46 20.896c-.68.327-1.464-.159-1.46-.867a.66.66 0 0 1 .033-.186L3.5 15" />
+                                </svg>
 
-                        </button>
+                            </button>
+                        </div>
 
 
 
