d73736b005e29e761a25de7e0f5db06d782ec8c0
namuakwembo <makwembonamu@gmail.com>
2026-03-20 21:17:15 +0300

    added wirechat:storage disk env variable

--- config/wirechat.php
+++ config/wirechat.php
@@ -40,7 +40,7 @@ return [
      |
      */
     'storage' => [
-        'disk' => 'public',
+        'disk' => env('WIRECHAT_STORAGE_DISK', 'public'),
         'visibility' => 'public',
         'directories' => [
             'attachments' => 'attachments',
