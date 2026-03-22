2460f36755e3107dfbb46a18be1bd1858f84f365
namuakwembo <makwembonamu@gmail.com>
2026-03-23 02:04:53 +0300

    fixed wirechat icon

--- resources/views/components/icon.blade.php
+++ resources/views/components/icon.blade.php
@@ -12,19 +12,18 @@
     $iconBag = $iconAttributes instanceof ComponentAttributeBag
         ? $iconAttributes
         : new ComponentAttributeBag(is_array($iconAttributes) ? $iconAttributes : []);
-
-    // Merge: caller attrs + icon attrs + defaults
-    $final = $attributes
-        ->merge($iconBag->getAttributes())
-        ->merge(['class' => 'size-6 text-zinc-700 dark:text-gray-100 dark:hover:text-gray-200']);
 @endphp
 
 @if ($icon instanceof Htmlable)
     {{ $icon }}
 @elseif (is_string($icon) && $icon !== '')
-    <x-dynamic-component :component="$icon" {{ $final }} />
+    <x-dynamic-component :component="$icon" {{ $attributes
+        ->merge($iconBag->getAttributes())
+        ->merge(['class' => 'size-6 text-zinc-700 dark:text-gray-100 dark:hover:text-gray-200']) }} />
 @elseif ($default instanceof Htmlable)
     {{ $default }}
 @elseif (is_string($default) && $default !== '')
-    <x-dynamic-component :component="$default" {{ $final }} />
+    <x-dynamic-component :component="$default" {{ $attributes
+        ->merge($iconBag->getAttributes())
+        ->merge(['class' => 'size-6 text-zinc-700 dark:text-gray-100 dark:hover:text-gray-200']) }} />
 @endif
