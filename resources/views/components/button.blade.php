67c10b2bcbade776a2dc3e66a0af12c08c696a08
namuakwembo <makwembonamu@gmail.com>
2026-05-04 01:09:18 +0300

    updaed fixed buttons

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -12,8 +12,8 @@
     $variantClasses = [
         'primary' => 'bg-[var(--primary-500)] text-white hover:opacity-90',
         'secondary' => 'text-gray-900 border border-zinc-200 dark:border-zinc-600/70 dark:text-white bg-white dark:bg-zinc-800/80 hover:bg-zinc-100/60 dark:hover:bg-zinc-800/90',
-        'default' => 'text-gray-900 border  border-zinc-200 dark:border-zinc-600/70 dark:text-white bg-white dark:bg-zinc-800/80  hover:bg-zinc-100/60 dark:hover:bg-zinc-800/90',
-        'filled' => 'text-gray-900   dark:text-white bg-zinc-200/60  hover:bg-zinc-100/60 bg-zinc-800/60  dark:hover:bg-zinc-800/90',
+        'default' => 'text-gray-900 border  border-zinc-200 dark:border-zinc-600/70 dark:text-white shadow-xs dark:shadow-none dark:bg-zinc-800/80  hover:bg-zinc-100/60 dark:hover:bg-zinc-800/90',
+        'filled' => 'text-gray-900   dark:text-white bg-zinc-200/60 dark:bg-zinc-800/60   hover:bg-zinc-100/60  dark:hover:bg-zinc-800/90',
         'outline' => 'border border-[var(--wc-light-border)] dark:border-[var(--wc-dark-border)] bg-transparent text-gray-900 dark:text-white hover:bg-[var(--wc-light-secondary)]/60 dark:hover:bg-[var(--wc-dark-secondary)]/60',
         'link' => 'rounded-none p-0 font-medium text-[var(--wc-brand-primary)] hover:underline',
         'subtle' => ' bg-transparent text-gray-500 dark:text-gray-400 hover:bg-zinc-300/60 dark:hover:bg-zinc-800/60',
