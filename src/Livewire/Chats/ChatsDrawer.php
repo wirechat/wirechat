0e73cfe4c89417e47bc98a4fc70edb47fcf4bb88
namuakwembo <makwembonamu@gmail.com>
2026-05-23 22:07:15 +0300

    security: harden wirechat modal components

--- src/Livewire/Chats/ChatsDrawer.php
+++ src/Livewire/Chats/ChatsDrawer.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Collection;
 use Illuminate\Support\Reflector;
 use Livewire\Component;
 use Livewire\Mechanisms\ComponentRegistry;
+use Wirechat\Wirechat\Livewire\Concerns\ModalComponent;
 
 class ChatsDrawer extends Component
 {
@@ -59,10 +60,14 @@ class ChatsDrawer extends Component
     {
         if (class_exists(ComponentRegistry::class)
             && app()->bound(ComponentRegistry::class)) {
-            return app(ComponentRegistry::class)->getClass($component);
+            $componentClass = app(ComponentRegistry::class)->getClass($component);
+        } else {
+            $componentClass = app('livewire.finder')->resolveClassComponentClassName($component);
         }
 
-        return app('livewire.finder')->resolveClassComponentClassName($component);
+        abort_unless(is_subclass_of($componentClass, ModalComponent::class), 403);
+
+        return $componentClass;
     }
 
     protected function getComponentName(string $class): string
